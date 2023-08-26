40bd0f79df2f3fd544d29ed656aee0f18b651999
unknown <doanquoc12c3>
2023-08-26 15:34:03 +0800

    final demo spring

--- src/main/java/net/doanquoc/springboot/controller/StudentController.java
+++ src/main/java/net/doanquoc/springboot/controller/StudentController.java
@@ -1,35 +1,37 @@
 package net.doanquoc.springboot.controller;
 
 import net.doanquoc.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
     // http://localhost:8080/student
     @GetMapping("student")
-    public Student getStudent(){
+    public ResponseEntity<Student> getStudent(){
         Student student = new Student(
                 1,
                 "Doan",
                 "Quoc"
         );
-        return student;
+        return ResponseEntity.ok().header("custom-header","doanquoc").body(student);
+        //return new ResponseEntity<>(student, HttpStatus.CONFLICT);
     }
 
     // http://localhost:8080/students
-    @GetMapping("students")
-    public List<Student> getStudents(){
+    @GetMapping("")
+    public ResponseEntity<List<Student>> getStudents(){
         List<Student> students = new ArrayList<>();
         students.add(new Student(1,"doan", "quoc"));
         students.add(new Student(2,"doan1", "quoc2"));
         students.add(new Student(3,"doan2", "quoc3"));
         students.add(new Student(4,"doan3", "quoc4"));
-        return students;
+        return ResponseEntity.ok(students);
     }
 
 
@@ -42,6 +44,55 @@ public class StudentController {
                                        @PathVariable("last-name") String lastName){
         return new Student(studentId,firstName, lastName);
     }
+
+    //Spring boot REST API with Request Param
+    // http://localhost:8080/students/query?id=1&firstName=doan&lastName=quoc
+    @GetMapping("query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id,firstName,lastName);
+    }
+
+    //Spring boot REST api that handles http post request -- create a new resources
+    // @PostMapping and @RequestBody
+    @PostMapping("create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+
+    //spring rest api that handles http put request -- updating existing resources
+    @PutMapping("{id}/update")
+
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId){
+        student.setId(studentId);
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    // spring boot rest api that handles http delete request - deleting the existing resources
+    @DeleteMapping("{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId){
+        System.out.println(studentId);
+        return "Student deleted successfully";
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
